8799b0c7f6bc1589a76451cd7866def3841361a1
Alexander Brodén <broden.alexander+github@gmail.com>
2012-09-25 11:21:34 +0200

    Avatar class

---
+++ CoreLib/src/Avatar.h
@@ -0,0 +1,16 @@
+#ifndef AVATAR_H
+#define AVATAR_H
+
+#include "GameObject.h"
+#include "TransformInfo.h"
+
+class Avatar : public GameObject
+{
+public:
+	Avatar();
+	Avatar(SpriteInfo* p_spriteInfo);
+
+	void update(float p_deltaTime, InputInfo p_inputInfo);
+};
+
+#endif
