c4f732acaef6ed8351babed90bb913b9f509e9f8
Hoodad <robin.thunstrom@gmail.com>
2012-10-02 12:15:09 +0200

    Updated SuperPill.cpp constructor to become compatible with the newly updated render pipeline.

--- CoreLib/src/SuperPill.cpp
+++ CoreLib/src/SuperPill.cpp
@@ -3,16 +3,16 @@
 
 SuperPill::SuperPill(IODevice* p_io, Tile* p_tile, GameStats* p_gameStats): Collectable(NULL,p_gameStats)
 {
-	SpriteInfo spriteInfo;
+	m_spriteInfo = new SpriteInfo();
 	TilePosition t = p_tile->getTilePosition();
 	float w = p_tile->getWidth();
 	float h = p_tile->getHeight();
-	spriteInfo.transformInfo.translation[TransformInfo::X] = t.x * w + w * 0.5f;
-	spriteInfo.transformInfo.translation[TransformInfo::Y] = t.y * h + h * 0.5f;
-	spriteInfo.transformInfo.scale[TransformInfo::X] = w * 0.7f;
-	spriteInfo.transformInfo.scale[TransformInfo::Y] = h * 0.7f;
-	spriteInfo.textureFileName = "..\\Textures\\SuperPill.png";
-	m_spriteInfo = p_io->addSpriteInfo(spriteInfo);
+	m_spriteInfo->transformInfo.translation[TransformInfo::X] = t.x * w + w * 0.5f;
+	m_spriteInfo->transformInfo.translation[TransformInfo::Y] = t.y * h + h * 0.5f;
+	m_spriteInfo->transformInfo.scale[TransformInfo::X] = w * 0.7f;
+	m_spriteInfo->transformInfo.scale[TransformInfo::Y] = h * 0.7f;
+	m_spriteInfo->textureFilePath = "..\\Textures\\SuperPill.png";
+	p_io->addSpriteInfo(m_spriteInfo);
 
 	m_tile = p_tile;
 	m_tile->addPill(this);
