813f89411eed239be680dc9a8896984d740046d1
Jarl <jarl.larsson@gmail.com>
2012-10-12 11:18:09 +0200

    Implemented all tests for GlyphMap.

--- TestEntry/src/Test_GlyphMap.h
+++ TestEntry/src/Test_GlyphMap.h
@@ -7,14 +7,30 @@
 class Test_GlyphMap: public Test
 {
 public:
-	Test_GlyphMap(): Test("GLYPHMAP")
+	Test_GlyphMap(): Test("GLYPHMAP_(ABC123)")
 	{
 	}
 	void setup()
 	{
 		GlyphMap glyphMap("ABC123", "dummy", 8, 8);
+
 		m_entries.push_back(TestData("TotalWidthInPixels",
 			glyphMap.getMaxBitmapWidth() == 6 * 8));
+
+		m_entries.push_back(TestData("GetCharRect_char(A)",
+			glyphMap.getCharRect('A').x == 0 &&
+			glyphMap.getCharRect('A').y == 0 &&
+			glyphMap.getCharRect('A').width == 8 &&
+			glyphMap.getCharRect('A').height == 8));
+
+		m_entries.push_back(TestData("GetCharRect_char(3)",
+			glyphMap.getCharRect('3').x == 5 * 8 &&
+			glyphMap.getCharRect('3').y == 0 &&
+			glyphMap.getCharRect('3').width == 8 &&
+			glyphMap.getCharRect('3').height == 8));
+
+		m_entries.push_back(TestData("getPath",
+			glyphMap.getPath() == "dummy"));
 	}
 
 };
