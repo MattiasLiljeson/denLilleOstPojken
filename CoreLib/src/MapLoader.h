e3b15737c05de162c56c6e294d8159a164049d8e
Hoodad <robin.thunstrom@gmail.com>
2012-10-16 14:50:12 +0200

    Added tons of stuff regarding the map parsing, also added compability with Tiled-map editor. Map 1 is now the original Pac Man.

--- CoreLib/src/MapLoader.h
+++ CoreLib/src/MapLoader.h
@@ -8,26 +8,32 @@
 #include "State.h"
 #include "GOFactory.h"
 
-
 using namespace std;
 
-enum TileType
-{
-	//Start couting on one not zero
-	PAD,
-	EMPTY,
-	WALL_CENTER,
-	PILL,
-	TRAP,
-	AVATAR_SPAWN,
-	MONSTER_SPAWN,
-	SUPERPILL,
-	SPEEDPILL,
-	
-	SWITCH = 31,
-	WALLSWITCH = 61
-};
 
+enum TileTypes
+{
+	//All equals or below 90 will be regard as walls
+	WALLS = 90,
+	//All equals or below  will be regard as paths
+	PATHS = 180,
+	//All equals or below  will be regard as SWITCHES
+	SWITCHES = 210,
+	//All equals or below  will be regard as WALLSWITCHES
+	WALLSWITCHES = 240,
+	//The chess-boy spawn
+	CBSPAWN = 271,
+	//All equals or below  will be regard as EnemieSpawn
+	ENEMIESPAWN = 330,
+	//All equals or below  will be regard as Buffs
+	BUFFS = 390,
+	//All equals or below  will be regard as Item
+	ITEMS = 420,
+	//All equals or below  will be regard as EatAblePowerUps
+	EATPOWERUP = 450,
+	//All equals or below  will be regard as Traps
+	TRAPS = 480
+};
 class MapLoader
 {
 private:
@@ -36,9 +42,13 @@ private:
 	Avatar*				m_avatar;
 	vector<Monster*>	m_monsters;
 	vector<Trap*>		m_traps;
-	GameStats* m_stats;
-	GOFactory*	m_factory;
-
+	GameStats*			m_stats;
+	GOFactory*			m_factory;
+	int					m_theme, 
+						m_width, 
+						m_height;
+private:
+	void parseHead(fstream &p_file);
 public:
 	MapLoader();
 	virtual ~MapLoader();
