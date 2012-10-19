cb2a0475a2d28096ab6a83ac62356f3a34c32283
Hoodad <robin.thunstrom@gmail.com>
2012-10-19 14:02:41 +0200

    Added tutorial maps

--- CoreLib/src/InGameState.cpp
+++ CoreLib/src/InGameState.cpp
@@ -52,7 +52,7 @@ bool InGameState::onEntry()
 			m_factory = new GOFactory(m_io);
 			m_tileMap = NULL;
 			m_stats = NULL;
-			m_currentMap = 1;
+			m_currentMap = 0;
 			m_gui = NULL;
 		}
 		restart();
@@ -95,7 +95,7 @@ void InGameState::update(float p_dt)
 		
 		if (m_stats->getNumPills() < 1)
 		{
-			m_currentMap = 1;//(m_currentMap+1) % 2;
+			m_currentMap = (m_currentMap+1) % 4;
 			restart();
 			return;
 		}
