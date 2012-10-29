90c47ec3cf4b5b630cdab21e9ec9c709d0763749
Jarl <jarl.larsson@gmail.com>
2012-10-29 11:39:54 +0100

    Shadow texture added

--- CoreLib/src/GOFactory.cpp
+++ CoreLib/src/GOFactory.cpp
@@ -22,7 +22,7 @@ Avatar* GOFactory::CreateAvatar(Tilemap* p_map, Tile* p_startTile, GameStats* p_
 	r.height = 64;
 	SpriteInfo* spriteInfo = CreateSpriteInfo("../Textures/player.png",
 		pos, size, &r);
-	SpriteInfo* shadow = CreateSpriteInfo("../Textures/player.png",
+	SpriteInfo* shadow = CreateSpriteInfo("../Textures/playerShadow.png",
 		pos, size, &r);
 	return new Avatar(spriteInfo, shadow, p_map, p_startTile, p_stats, CreateSoundInfo("../Sounds/avatar_killed.wav",100), CreateSoundInfo("../Sounds/jump.wav",100));
 }
